84c8414cbd0fb004dc7846600860cbd1c6d0c8a5
Pavan Gole <pavangoley5@gmail.com>
2023-04-06 23:41:47 +0530

    Added proper configuration paths

--- app/databases/database.py
+++ app/databases/database.py
@@ -6,9 +6,10 @@ import os
 
 database_url = os.environ.get('url')
 password = os.environ.get('password')
+cert_path = os.environ.get('cert_path')
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://pavangole:{password}@{database_url}/frs"
 engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={
-    "ssl_ca": "/home/pavangole/workspace-Pavan/Projects/certificate/DigiCertGlobalRootCA.crt.pem"
+    "ssl_ca": f"{cert_path}/DigiCertGlobalRootCA.crt.pem"
 })
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base = declarative_base()
